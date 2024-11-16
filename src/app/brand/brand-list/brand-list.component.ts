223809c3be42b20a9eaa4ec1f9574da947f0fcfd
anhtai2210 <ng.atai.2210@gmail.comm>
2024-11-16 12:11:07 +0700

    Brands-suppliers

--- src/app/brand/brand-list/brand-list.component.ts
+++ src/app/brand/brand-list/brand-list.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { BrandService } from '../brand.service';
 import { Brand } from '../brand.model';
 import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-brand-list',
@@ -9,24 +14,40 @@ import { Router } from '@angular/router';
 })
 export class BrandListComponent implements OnInit {
   brands: Brand[] = [];
+  errorMessage: string | null = null;
 
-  constructor(private brandService: BrandService, private router: Router) {}
-
+  constructor(private brandService: BrandService, private router: Router,  private apiService: ApiService, private http: HttpClient) {}
+  private endpoint = 'brands';
   ngOnInit(): void {
-    this.getBrands();
+    this.loadBrands();
   }
 
-  // Lấy danh sách tất cả thương hiệu
-  getBrands(): void {
+  // // Lấy danh sách tất cả thương hiệu
+  // getBrands(): void {
+  //   this.brandService.getAllBrands().subscribe(
+  //     (brands) => {
+  //       this.brands = brands;
+  //       console.log(brands)
+  //     },
+  //     (error) => {
+  //       console.error('Lỗi khi lấy danh sách thương hiệu:', error);
+  //     }
+  //   );
+  // }
+  loadBrands(): void {
     this.brandService.getAllBrands().subscribe(
       (brands) => {
         this.brands = brands;
+        console.log(brands)
+        this.errorMessage = null;
       },
       (error) => {
-        console.error('Lỗi khi lấy danh sách thương hiệu:', error);
+        console.error('Error loading brands:', error);
+        this.errorMessage = 'Failed to load brands: ' + error.message;
       }
     );
   }
+
 
   // Xóa thương hiệu
   deleteBrand(id: string): void {
@@ -34,7 +55,7 @@ export class BrandListComponent implements OnInit {
       this.brandService.deleteBrand(id).subscribe(
         () => {
           console.log('Thương hiệu đã được xóa');
-          this.getBrands();  // Cập nhật lại danh sách sau khi xóa
+          this.loadBrands();  // Cập nhật lại danh sách sau khi xóa
         },
         (error) => {
           console.error('Lỗi khi xóa thương hiệu:', error);
