679d07f355939da20610c3914cd82b2012777c08
dangnt1301 <dangthanh515asd@gmail.com>
2024-12-07 15:55:00 +0700

    Fix-dashboard-dang

--- src/app/demo/dashboard/dashboar.service.ts
+++ src/app/demo/dashboard/dashboar.service.ts
@@ -18,4 +18,7 @@ export class StatisticsService {
   getYearlySales(): Observable<any[]> {
     return this.http.get<any[]>(`http://localhost:8080/api/statistics/yearly`);
   }
+  getProductsSold(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:8080/api/statistics/products-sold`);
+  }
 }
